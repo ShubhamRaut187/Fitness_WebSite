c8eb24113201738cd538b4efe36a66c03b2a581c
ShubhamRaut187 <raut.shubham4554@gmail.com>
2023-05-18 11:21:51 +0530

    TrainWithUs Page Added 18/05/2023

--- src/index.js
+++ src/index.js
@@ -27,6 +27,12 @@ let SlideShow = ()=>{
 }
 SlideShow();
 
+
+//Navbar Functions.
+let gotoHome = ()=>{
+    window.location="index.html";
+}
+let Home = document.getElementById("navbar_logo").addEventListener("click",gotoHome);
 let gotoTrainWithUs = ()=>{
     window.location = "trainwithus.html";
 }
